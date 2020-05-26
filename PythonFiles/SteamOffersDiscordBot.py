0121b1d20f011669215ba0c464df6cbbba8e441c
AllanCapistrano <allan.capistrano3@gmail.com>
2020-05-26 16:32:09 -0300

    Criação dos comando $promocao e  $botinfo

--- PythonFiles/SteamOffersDiscordBot.py
+++ PythonFiles/SteamOffersDiscordBot.py
@@ -3,29 +3,8 @@ import asyncio
 import discord
 import discordToken
 
-#Convite discordapp.com/oauth2/authorize?client_id=714852360241020929&scope=bot&permissions=485440
-
-'''if __name__ == '__main__':
-    catchOffers = CatchOffers()
-
-    testeURL, testeIMG = catchOffers.getDailyGamesOffers()
-    testeOri, testeFin = catchOffers.getDailyGamesOffersPrices()
-
-    print('URL: ' + str(testeURL))
-    print('Imagem: ' + str(testeIMG))
-    print('Original: ' + str(testeOri))
-    print('Final: ' + str(testeFin))
-
-    testeURL_2, testeIMG_2 = catchOffers.getSpotlightOffers()
-
-    print('URL: ' + str(testeURL_2) + '\n')
-    print('Imagem: ' + str(testeIMG_2) + '\n')
-
-    print('H2: ' + str(catchOffers.getSpotlightOffersContentH2()))'''
-
 TOKEN = discordToken.myToken()
 client = discord.Client()
-member = discord.Member #Verificar se vai precisar
 color = 0xa82fd2
 icon = "https://cdn.discordapp.com/app-icons/714852360241020929/b8dcc72cfc7708a4efd31787dceb5350.png?size=64"
 
@@ -46,7 +25,7 @@ async def on_message(message):
         embedHelp.add_field(name = "```$promocao```", value = "**Exibe quais jogos estão na promoção diária da Steam ou gratuitos por um tempo limitado.**", inline = False)
         embedHelp.add_field(name = "```$destaque```", value = "**Exibe os eventos que estão em destaque na Steam, ou os jogos em promoção que estão em destaque na loja.**", inline = False)
         embedHelp.add_field(name = "```$convite```", value = "**Gera o convite para que o Bot possa ser adicionado em outros servidores.**", inline = False)
-        embedHelp.add_field(name = "```$botinfo```", value = "**Exibe as informações do Bot e do criador do mesmo.**", inline = False)
+        embedHelp.add_field(name = "```$botinfo```", value = "**Exibe as informações do Bot.**", inline = False)
 
         await message.channel.send(embed = embedHelp)
 
@@ -63,7 +42,6 @@ async def on_message(message):
     if(message.content.lower().startswith("$destaque")):
         catchOffers = CatchOffers()
         list_gamesURl, list_gamesIMG, list_H2 = catchOffers.getSpotlightOffers()
-        #list_H2 = catchOffers.getSpotlightOffersContentH2()
         x = len(list_gamesURl)
 
         while(x > 0):
@@ -81,6 +59,36 @@ async def on_message(message):
 
     if(message.content.lower().startswith("$promocao")):
         catchOffers = CatchOffers()
+        list_gamesURl, list_gamesIMG = catchOffers.getDailyGamesOffers()
+        list_gamesOP, list_gamesFP = catchOffers.getDailyGamesOffersPrices()
+        x = len(list_gamesURl)
+        print("debug: " + str(x)) #Apagar depois
 
+        while(x > 0):
+            embedDailyGames = discord.Embed(
+                title = "🕹️ Oferta do Dia 🕹️",
+                color = color
+            )
+            embedDailyGames.set_image(url = list_gamesIMG[x - 1])
+            embedDailyGames.add_field(name = "**Link:**", value = "**" + list_gamesURl[x - 1] + "**", inline = False)
+            embedDailyGames.add_field(name = "**Preço Original:**", value = "**" + list_gamesOP[x - 1] + "**", inline = True)
+            embedDailyGames.add_field(name = "**Preço com Desconto:**", value = "**" + list_gamesFP[x - 1] + "**", inline = True)
+            embedDailyGames.set_footer()
+
+            await message.channel.send(embed = embedDailyGames)
+
+            x = x - 1
+
+    if(message.content.lower().startswith("$botinfo")):
+        embedBotInfo = discord.Embed(
+            title = "📊 Informações 📊",
+            color = color
+        )
+        embedBotInfo.add_field(name = "Python", value = "**3.7.7**", inline = True)
+        embedBotInfo.add_field(name = "discord.py", value = "**1.3.3**", inline = True)
+        embedBotInfo.add_field(name = "Sobre SteamOffersBot", value = "**Bot feito para notificar os jogos que estão em promoção, sem a necessidade de abrir a Steam ou sair do Discord. Foi criado por ArticZ#1081**", inline = False)
+        embedBotInfo.set_footer(text = "Criado em 26 de Maio de 2020!")
+
+        await message.channel.send(embed = embedBotInfo)
 
 client.run(TOKEN)
