ea48e65747b86d365190d7b56638038a9dfafbc7
AllanCapistrano <allan.capistrano3@gmail.com>
2022-01-23 16:34:44 -0300

    Update commandAlert.

--- src/services/messages.py
+++ src/services/messages.py
@@ -202,9 +202,9 @@ class Message:
         alertList = []
 
         alertList.append("⚠️ **Informe o nome do jogo! Ex: `{}game undertale`**".format(prefix))
-        alertList.append("⚠️ **Informe o gênero do jogo! Ex: `{}genre casual`**".format(prefix))
+        alertList.append("⚠️ **Informe o gênero do jogo! Ex: `{0}genre casual` \n Ou digite `{0}help genre` para ver todos os gêneros.**".format(prefix))
         alertList.append("⚠️ **Comando inválido!**")
-        alertList.append("⚠️ **Informe a categoria! Ex: `{0}gametab pré-venda` \n Ou digite `{0}help gametab`  para ver todas as categorias.**".format(prefix))
+        alertList.append("⚠️ **Informe a categoria! Ex: `{0}gametab pré-venda` \n Ou digite `{0}help gametab` para ver todas as categorias.**".format(prefix))
 
         return alertList
 
