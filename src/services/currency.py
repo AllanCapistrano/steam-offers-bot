b5862e972fb595ed09808282f4363fa7992b075d
AllanCapistrano <allan.capistrano3@gmail.com>
2022-01-25 21:10:51 -0300

    Add constructor.

---
+++ src/services/currency.py
@@ -0,0 +1,62 @@
+class Currency:
+    def __init__(self):
+        self.currencies = [
+        "AED",
+        "ARS",
+        "AUD",
+        "BRL",
+        "CAD",
+        "CHF",
+        "CLP",
+        "CNY",
+        "COP",
+        "CRC",
+        "EUR",
+        "GBP",
+        "HKD",
+        "ILS",
+        "IDR",
+        "INR",
+        "JPY",
+        "KRW",
+        "KWD",
+        "KZT",
+        "MXN",
+        "MYR",
+        "NOK",
+        "NZD",
+        "PEN",
+        "PHP",
+        "PLN",
+        "QAR",
+        "RUB",
+        "SAR",
+        "SGD",
+        "THB",
+        "TRY",
+        "TWD",
+        "UAH",
+        "USD",
+        "UYU",
+        "VND",
+        "ZAR"
+    ]
+        self.defaultCurrency = "BRL"
+
+    def currencyExists(self, c: str) -> bool:
+        """ Verifica se a moeda passada esta disponível na Steam.
+
+        Parameters
+        -----------
+        c: :class:`str`
+            Moeda digitada.
+
+        Returns
+        -----------
+        Boolean: :class:`bool`
+        """
+        
+        for currency in self.currencies:
+            if(c == currency):
+                return True
+        return False
