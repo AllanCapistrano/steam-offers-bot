0c812c4f32c29c7c4d7af82bffe4ffd5395faa7b
AllanCapistrano <allan.capistrano3@gmail.com>
2022-02-01 18:54:06 -0300

    Using EmbedSpecificGame class.

--- src/services/SpecificGame/specificGameEmbed.py
+++ src/services/SpecificGame/specificGameEmbed.py
@@ -1,9 +1,9 @@
 from typing import Literal
-import discord
 from discord.embeds import Embed
 
 from services.crawler import Crawler
 from services.messages import Message
+from embeds.embedSpecificGame import EmbedSpecificGame
 
 async def specificGameEmbed(
     crawler: Crawler,
@@ -52,51 +52,18 @@ async def specificGameEmbed(
         gameOriginalPrice != None and
         gameFinalPrice    != None
     ):
-        message = Message()
-
-        embedSpecificGame = discord.Embed(
-            title = message.title(gameName=gameName)[5],
-            color = embedColor
+        embedSpecificGame = EmbedSpecificGame(
+            color             = embedColor,
+            gameName          = gameName,
+            gameImg           = gameIMG,
+            gameUrl           = gameURL,
+            gameOriginalPrice = gameOriginalPrice,
+            gameFinalPrice    = gameFinalPrice,
+            gameDescription   = gameDescription,
+            searchUrl         = searchUrl,
+            message           = Message()
         )
-        embedSpecificGame.set_image(url=gameIMG)
-        embedSpecificGame.add_field(
-            name   = "**Link:**", 
-            value  = "**[Clique Aqui]({})**".format(gameURL), 
-            inline = False
-        )
 
-        if(gameOriginalPrice != gameFinalPrice): # Caso o jogo esteja em promoção.
-            embedSpecificGame.add_field(
-                name   = "**Preço Original:**", 
-                value  = "**{}**".format(gameOriginalPrice), 
-                inline = True
-            )
-            embedSpecificGame.add_field(
-                name   = "**Preço com Desconto:**", 
-                value  = "**{}**".format(gameFinalPrice), 
-                inline = True
-            )
-        else: # Caso o jogo não esteja em promoção.
-            embedSpecificGame.add_field(
-                name   = "**Preço:**", 
-                value  = "**{}**".format(gameOriginalPrice), 
-                inline = False
-            )
-
-        if(gameDescription != None):
-            embedSpecificGame.add_field(
-                name   = "**Descrição:**", 
-                value  = "{}".format(gameDescription), 
-                inline = False
-            )
-
-        if(searchUrl != None): # Caso seja passado o nome do jogo.
-            embedSpecificGame.add_field(
-                name   = "**Obs:**", 
-                value  = message.wrongGame(searchUrl), 
-                inline = False
-            )
-
-        return embedSpecificGame
+        return embedSpecificGame.embedSpecificGamePortuguese()
     else:
         return None
