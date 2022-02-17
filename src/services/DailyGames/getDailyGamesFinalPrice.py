169d008658d4849c11cbff7aa2ca7a410a7d3b48
AllanCapistrano <allan.capistrano3@gmail.com>
2022-02-17 17:28:20 -0300

    Add english support.

--- src/services/DailyGames/getDailyGamesFinalPrice.py
+++ src/services/DailyGames/getDailyGamesFinalPrice.py
@@ -1,12 +1,14 @@
 from bs4 import BeautifulSoup
 
-def getDailyGamesFinalPrice(soup: BeautifulSoup) -> list:
+def getDailyGamesFinalPrice(soup: BeautifulSoup, language: str) -> list:
     """ Função responsável por retornar uma lista contendo os preços com o 
     desconto aplicado dos jogos que estão em promoção.
 
     Parameters
     -----------
     soup: :class:`BeautifulSoup`
+    language: :class:`str`
+        Idioma que se deseja visualizar a página do jogo.
 
     Returns
     -----------
@@ -17,9 +19,12 @@ def getDailyGamesFinalPrice(soup: BeautifulSoup) -> list:
 
     for dailyGames in soup.find_all("div", class_="dailydeal_ctn"):
         dailyGamesPricesBlock = dailyGames.find_all("div", class_="discount_prices")
-        
+
         if(len(dailyGamesPricesBlock) == 0):
-            finalPrices.append("Não disponível!")
+            if(language == "brazilian"):
+                finalPrices.append("Não disponível!")
+            elif(language == "english"):
+                finalPrices.append("Not available!")
         else:
             for dailyGamesPrices in dailyGamesPricesBlock:
                 finalPriceDiv = dailyGamesPrices.find_all("div", class_="discount_final_price")
@@ -27,6 +32,9 @@ def getDailyGamesFinalPrice(soup: BeautifulSoup) -> list:
                 if(len(finalPriceDiv) > 0):
                     finalPrices.append(finalPriceDiv[0].contents[0])
                 else:
-                    finalPrices.append("Não disponível!")
+                    if(language == "brazilian"):
+                        finalPrices.append("Não disponível!")
+                    elif(language == "english"):
+                        finalPrices.append("Not available!")
         
     return finalPrices
