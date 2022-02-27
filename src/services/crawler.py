a1142807348109250d98bd94e69c1a2bdf96a511
AllanCapistrano <allan.capistrano3@gmail.com>
2022-02-27 18:49:04 -0300

    Formatting.

--- src/services/crawler.py
+++ src/services/crawler.py
@@ -46,10 +46,10 @@ from services.GameReview.getReviewTotalAmount import getReviewTotalAmount
 from services.GameReview.getReviewSumary import getReviewSumary
 
 # ------------------------------ Constants ----------------------------------- #
-URL_MAIN = "https://store.steampowered.com/?cc=br&l=brazilian"
-URL_SPECIALS = "https://store.steampowered.com/specials?"
-URL_GAME = "https://store.steampowered.com/search/?"
-URL_GENRE = "https://store.steampowered.com/category/"
+URL_MAIN        = "https://store.steampowered.com/?cc=br&l=brazilian"
+URL_SPECIALS    = "https://store.steampowered.com/specials?"
+URL_GAME        = "https://store.steampowered.com/search/?"
+URL_GENRE       = "https://store.steampowered.com/category/"
 URL_PRICE_RANGE = "https://store.steampowered.com/search/?"
 # ---------------------------------------------------------------------------- #
 class Crawler:
