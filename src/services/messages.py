bf1d4aafdc274f04ab15b82a385c141b75445490
AllanCapistrano <allan.capistrano3@gmail.com>
2022-03-02 11:52:06 -0300

    Update infoValues.

--- src/services/messages.py
+++ src/services/messages.py
@@ -204,11 +204,11 @@ class Message:
         if(language == None):
             msgList.append("**Bot para visualizar informações sobre jogos e promoções "
                 "na Steam sem precisar sair do Discord. Criado por ") # Informações.
-            msgList.append("21 de Fevereiro de 2022") # Data da última atualização.
+            msgList.append("02 de Março de 2022") # Data da última atualização.
         elif(language == "english"):
             msgList.append("**Bot to show information about Steam games and "
                 "offers, without leaving Discord. Created by ") # Informações.
-            msgList.append("February 21, 2022") # Data da última atualização.
+            msgList.append("March 02, 2022") # Data da última atualização.
 
         return msgList
 
